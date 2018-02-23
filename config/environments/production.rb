72ce1363ab743570d16d11defb0806bb1f18e34c
Sébastien Erfani <sebastien.erfani@gmail.com>
2018-02-23 21:42:33 +0100

    Add reck(t) logo, add cache control headers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,11 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, s-maxage=#{365.days.to_i}, maxage=#{180.days.to_i}",
+    'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
+  }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
