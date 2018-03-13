27135e828b78ae88b883aa8d64532e9e04447824
Aruisehu <Yohann.JOLAIN@supinfo.com>
2018-03-14 00:12:55 +0100

    Fixed NilClass error

--- app/models/formula_template.rb
+++ app/models/formula_template.rb
@@ -1,11 +1,6 @@
 class FormulaTemplate < ApplicationRecord
     has_one :formula
     translates :name
-=begin
-    alias_method :starter?, :has_starter
-    alias_method :dish?, :has_dish
-    alias_method :dessert?, :has_dessert
-=end
 
     def starter?
         self.has_starter && !self.has_dish && !self.has_dessert
