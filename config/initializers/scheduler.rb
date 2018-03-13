7ffeaf995e87988897e2063a2a00a0bc3c66ff72
Sébastien Erfani <sebastien.erfani@gmail.com>
2018-03-13 10:34:15 +0100

    Fix missing end in sheduler.rb

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -8,9 +8,10 @@ Rails.configuration.after_initialize do
         scheduler.every '5m' do
             services = Service.opened
             services.each do |s|
-            if (s.start_time - 10.minutes).past?
-                s.ended = true
-                s.save
+                if (s.start_time - 10.minutes).past?
+                    s.ended = true
+                    s.save
+                end
             end
         end
     end
