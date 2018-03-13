4eee4e63b3e4437801e3b5769bc717f5275b5613
Aruisehu <Yohann.JOLAIN@supinfo.com>
2018-03-13 10:05:58 +0100

    Can desactivate services job

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,14 +2,16 @@ require 'rufus-scheduler'
 
 Rails.configuration.after_initialize do
 
-    scheduler = Rufus::Scheduler.new
+    if ENV["CLOSE_SERVICES"] == "true"
+        scheduler = Rufus::Scheduler.new
 
-    scheduler.every '5m' do
-        services = Service.opened
-        services.each do |s|
-            if (s.start_time - DateTime.now).to_f * 24 * 60 > 10
-                s.ended = true
-                s.save
+        scheduler.every '5m' do
+            services = Service.opened
+            services.each do |s|
+                if (s.start_time - DateTime.now).to_f * 24 * 60 > 10
+                    s.ended = true
+                    s.save
+                end
             end
         end
     end
