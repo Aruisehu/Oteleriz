df5dbd5cc7b10b1a8472689b69fa29314912b259
bsulyan <bastien.sulyan@gmail.com>
2018-03-14 14:37:33 +0100

    add admin layotu

--- app/controllers/admin/meals_controller.rb
+++ app/controllers/admin/meals_controller.rb
@@ -42,6 +42,6 @@ class Admin::MealsController < Admin::BaseController
     end
 
     def meal_params
-      params.require(:meal).permit(:name_en, :name_fr, :description_fr, :description_en, :price, :img_url)
+      params.require(:meal).permit(:end_time, :start_time)
     end
 end
