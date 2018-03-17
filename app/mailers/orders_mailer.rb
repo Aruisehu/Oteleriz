6ea087d2808c5e62258b11c34dc79573f5a7fe0e
Sébastien Erfani <sebastien.erfani@gmail.com>
2018-03-17 21:36:24 +0100

    Add mail content

--- app/mailers/orders_mailer.rb
+++ app/mailers/orders_mailer.rb
@@ -1,6 +1,6 @@
 class OrdersMailer < ApplicationMailer
     def confirm(order)
         @order = order
-        mail(to: @order.email, subject: 'Cook Me Up: Confirmation de votre commande')
+        mail(to: @order.email, subject: 'Cook Me Up: Confirmation de commande pour "' + @order.name + '"')
     end
 end
