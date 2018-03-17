d8e2fd16b8013dd01452a226d3211e3176ddc1cb
Aruisehu <Yohann.JOLAIN@supinfo.com>
2018-03-17 20:05:12 +0100

    Test mail round 6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,15 +66,15 @@ Rails.application.configure do
     # config.active_job.queue_adapter     = :resque
     # config.active_job.queue_name_prefix = "Oteleriz_#{Rails.env}"
     config.action_mailer.perform_caching = false
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-        address:              'ssl0.ovh.net',
-        port:                 465,
-        domain:               'cookmeup.restaurant',
-        user_name:            'cookmeupstartup@gmail.com',
-        password:             ENV['SMTP_PASSWORD'],
-        authentication:       'plain',
-        enable_starttls_auto: true  }
+    #config.action_mailer.delivery_method = :smtp
+    #config.action_mailer.smtp_settings = {
+    #    address:              'ssl0.ovh.net',
+    #    port:                 465,
+    #    domain:               'cookmeup.restaurant',
+    #    user_name:            'cookmeupstartup@gmail.com',
+    #    password:             ENV['SMTP_PASSWORD'],
+    #    authentication:       'plain',
+    #    enable_starttls_auto: true  }
 
 
     # Ignore bad email addresses and do not raise email delivery errors.
