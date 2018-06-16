078c275c350d5788154d3054581f1da24b58448c
Sébastien Erfani <sebastien.erfani@supinfo.com>
2018-06-16 22:19:29 +0200

    Change plain to :login smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
         address: "smtp.gmail.com",
         port: 587,
         domain: "cookmeup.restaurant",
-        authentication: "plain",
+        authentication: :login,
         enable_starttls_auto: true,
         user_name: ENV["GMAIL_USERNAME"],
         password: ENV["GMAIL_PASSWORD"]
